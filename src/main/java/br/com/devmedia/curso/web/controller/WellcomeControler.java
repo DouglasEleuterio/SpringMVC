71f6ecb4a7d50b0a75ea5ce72bfb55ef347b9edd
DouglasEleuterio <douglas.versato@gmail.com>
2017-10-02 16:35:38 -0300

    Criação do Controler e Welcome JSP.

---
+++ src/main/java/br/com/devmedia/curso/web/controller/WellcomeControler.java
@@ -0,0 +1,28 @@
+package br.com.devmedia.curso.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/*
+ * INformamos ao Spring que está é uma classe de Controler
+ */
+@Controller
+public class WellcomeControler {
+	
+	/*
+	 * Criando o metodo que abre a página wellcome.
+	 */
+	//Informamos que é este o metodo que página deve acessar.
+	@RequestMapping(value = "/", method = RequestMethod.GET)//Qual Path representa esse metodo. Acessou a / Cai neste metodo
+	// Method Get com verbo Http, como é consulta usamos o GET. GET é o atributo padrão do Method, caso não informemos o metodo.
+	// Por padrão ele define sendo como Get
+	
+	public String wellcome(){
+		// String com o nome da página. 
+		//Não precisamos informar a extensão da página por que já o informamos nas Configurações 
+		//o metodo responsável é o Resolver
+		
+		//O caminho WEB-INF/views/ Já está configurado no prefixo da Classe SpringMvcConfig
+		return "welcome";
+	}
+}
