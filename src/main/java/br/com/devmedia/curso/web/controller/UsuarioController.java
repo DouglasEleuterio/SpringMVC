3ea27d8f4c5378c0e102d8ed7dc9b990f4f0f790
DouglasEleuterio <douglas.vesato@gmail.com>
2017-10-02 23:16:08 -0300

    Finalização do Metodo de cadastro de Pessoas.

--- src/main/java/br/com/devmedia/curso/web/controller/UsuarioController.java
+++ src/main/java/br/com/devmedia/curso/web/controller/UsuarioController.java
@@ -3,11 +3,16 @@ package br.com.devmedia.curso.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.devmedia.curso.dao.UsuarioDao;
+import br.com.devmedia.curso.domain.Usuario;
 
 @Controller
 @RequestMapping("usuario")
@@ -17,9 +22,27 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioDao dao;
 	
+	//Parte de listagem de Usuarios
 	@RequestMapping(value = "/todos", method = RequestMethod.GET)
 	public ModelAndView listaTodos(ModelMap model) {
 		model.addAttribute("usuarios",dao.getTodos());
 		return new ModelAndView("/user/list", model);
+	}
+	
+	//Parte de cadastro de novos usuarios
+	
+	//Metodo para direcionar para página de cadastro
+	@GetMapping("/cadastro")
+	public String cadastro(@ModelAttribute("usuario")Usuario usuario, ModelMap model) {
+		return "/user/add";
+	}
+
+	
+	//Metodo para receber o formulario que sera enviado
+	@PostMapping("/save")
+	public String save(@ModelAttribute("usuario")Usuario usuario, ModelMap model) { //Se eu passar a mesagem via ModelMap ela não chegará até a página, por que este metodo não devolve objetos como resposta {
+		dao.salvar(usuario);
+		//attr.addFlashAttribute("message", "Usuário salvo com sucesso.");//Enviar no redirect qualquer tipo de váriavel
+		return "redirect:/usuario/todos";
 	}
 }
--- src/main/webapp/WEB-INF/views/user/add.jsp
+++ src/main/webapp/WEB-INF/views/user/add.jsp
@@ -14,24 +14,27 @@
 		<h1>Cadastro de Usuários</h1>
 		<hr>
 		<div>
-
-			<a class="btn btn-default" href="#">Home</a>
+			<spring:url value="/usuario/todos" var="home"/>
+			<a class="btn btn-default" href="${home }">Home</a>
 		</div>
 		<hr>
 		<div>
 			<!-- Atalho até o Controller que usaremos, caso usassemos o Link, teria que passar toda URL HTTp...UltimoPath -->
-			<spring:url value="usuario/save" var="save" /> <!-- /Usuario(RequestMapping) nosso controle / Save o Metodo de Usuario -->
+			<spring:url value="/usuario/save" var="save" /> <!-- /Usuario(RequestMapping) nosso controle / Save o Metodo de Usuario -->
 			<!--  Classe de Dominio USUARIO -->
 			<form:form modelAttribute="usuario" action="${save }" method="post">
+
 				<div class="form-group">
 					<label for="nome">Nome: </label>
 					<!-- PAth faz a ligação entre o formulário e Objeto de Dominio -->
 					<form:input path="nome" class="form-control" />
 				</div>
+				
 				<div class="form-group">
 					<label for="sobrenome">Sobrenome: </label>
 					<form:input path="sobrenome" class="form-control" />
 				</div>
+				
 				<div class="form-group">
 					<button type="submit" class="btn btn-primary">Confirmar</button>
 				</div>
